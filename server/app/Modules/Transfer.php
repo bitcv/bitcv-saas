8b2370e90ae42d43504a06ef2f0c6c7cbac1cb02
liuzhihua <zhihua@ucai.cn>
2018-02-06 19:46:23 +0800

    change index page content

---
+++ server/app/Modules/Transfer.php
@@ -0,0 +1,9 @@
+<?php
+
+class Transfer {
+    
+    public function tran() {
+
+    }
+    
+}
--- server/resources/views/proj/index.blade.php
+++ server/resources/views/proj/index.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.proj')
 
 @section('content')
-申请自己的SaaS平台<br>
+SaaS提供功能丰富的模块：<br>
+1 钱包地址收集<br>
+2 Token代发<br>
+3 糖果盒<br>
+
+<br>
+更多资讯服务请联系：<br>
+白皮书中英日韩翻译<br>
+网站搭建<br>
+contact@<br>
+
+<br>
+免费申请自己独立的SaaS平台<br>
 <a href="{{ route('proj.apply') }}">apply</a>
 @stop
