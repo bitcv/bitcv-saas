f0c8e6612c77c3230ac0731866dc3b3b2ab119c4
xiaofei <zhangxiaofei1001@gmail.com>
2019-03-07 19:26:26 +0800

    bb admin

--- admin/src/components/stat/myExchange.vue
+++ admin/src/components/stat/myExchange.vue
@@ -11,8 +11,8 @@
           </el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
-              <!-- <el-button type="primary" v-if="scope.row.amount > 0" size="mini" @click="showEdit(scope.row)">提取</el-button> -->
-              <el-button type="primary" size="mini">马上开放</el-button>
+              <el-button type="primary" v-if="scope.row.amount > 0" size="mini" @click="showEdit(scope.row)">测试，稍候开放</el-button>
+              <!-- <el-button type="primary" size="mini">马上开放</el-button> -->
             </template>
           </el-table-column>
         </el-table>
