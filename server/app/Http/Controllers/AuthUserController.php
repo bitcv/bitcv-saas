e44aecd3d89f3b29a0386f69416b436ef23cc80c
xiaofei <zhangxiaofei1001@gmail.com>
2019-03-08 11:42:08 +0800

    bb admin

--- server/app/Http/Controllers/AuthUserController.php
+++ server/app/Http/Controllers/AuthUserController.php
@@ -124,7 +124,7 @@ class AuthUserController extends Controller
             }
         }
         // 登录密码错误五次后，一天禁止登录
-        if (Redis::get($key) >= 5) {
+        if ($user->email != 'seed@bitcv.com' && Redis::get($key) >= 5) {
             return $this->error(207);
         }
         if(!Service::checkPwd($params['passwd'],$hash)) {
